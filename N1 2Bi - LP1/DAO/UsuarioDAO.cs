5bcecd53beaba1f5a77f4743608c2df9dbaa02b8
Kauê de Souza Silva <145780136+ConfuseKarma@users.noreply.github.com>
2024-05-07 11:40:14 -0300

    Update UsuarioDAO.cs
    adicionando numero

--- N1 2Bi - LP1/DAO/UsuarioDAO.cs
+++ N1 2Bi - LP1/DAO/UsuarioDAO.cs
@@ -7,54 +7,57 @@ using System.Diagnostics;
 namespace N1_2Bi___LP1.DAO
 {
     public class UsuarioDAO : PadraoDAO<UsuarioViewModel>
+{
+    protected override SqlParameter[] CriaParametros(UsuarioViewModel model)
     {
-        protected override SqlParameter[] CriaParametros(UsuarioViewModel model)
-        {
-            SqlParameter[] parametros = new SqlParameter[5];
-            parametros[0] = new SqlParameter("nome", model.Nome);
-            parametros[1] = new SqlParameter("cpf", model.Cpf);
-            parametros[2] = new SqlParameter("telefone", model.Telefone);
-            parametros[3] = new SqlParameter("email", model.Email);
-            parametros[4] = new SqlParameter("endereco", model.Endereco);
+        SqlParameter[] parametros = new SqlParameter[6]; // Agora são 6 parâmetros
+        parametros[0] = new SqlParameter("nome", model.Nome);
+        parametros[1] = new SqlParameter("cpf", model.Cpf);
+        parametros[2] = new SqlParameter("telefone", model.Telefone);
+        parametros[3] = new SqlParameter("email", model.Email);
+        parametros[4] = new SqlParameter("endereco", model.Endereco);
+        parametros[5] = new SqlParameter("numero", model.Numero); // Adicionando o parâmetro para o número da casa
 
-            return parametros;
-        }
-
-        protected override UsuarioViewModel MontaModel(DataRow registro)
-        {
-            UsuarioViewModel usuario = new UsuarioViewModel();
-            usuario.Nome = registro["nome"].ToString();
-            usuario.Cpf = registro["cpf"].ToString();
-            usuario.Telefone = registro["telefone"].ToString();
-            usuario.Email = registro["email"].ToString();
-            usuario.Endereco = registro["endereco"].ToString();
-            return usuario;
-        }
-
-        public void Inserir(UsuarioViewModel usuario)
-        {
-            HelperDAO.ExecutaProc("spInsert_Usuarios", CriaParametros(usuario));
-        }
-
-        public void Alterar(UsuarioViewModel usuario)
-        {
-            HelperDAO.ExecutaProc("spUpdate_Usuarios", CriaParametros(usuario));
-        }
-
-        public void Excluir(int id)
-        {
-            var p = new SqlParameter[]
-            {
-        new SqlParameter("id", id)
-            };
-            HelperDAO.ExecutaProc("spDelete", p);
-        }
-
-
-        protected override void SetTabela()
-        {
-            Tabela = "Usuarios";
-        }
+        return parametros;
     }
+
+    protected override UsuarioViewModel MontaModel(DataRow registro)
+    {
+        UsuarioViewModel usuario = new UsuarioViewModel();
+        usuario.Nome = registro["nome"].ToString();
+        usuario.Cpf = registro["cpf"].ToString();
+        usuario.Telefone = registro["telefone"].ToString();
+        usuario.Email = registro["email"].ToString();
+        usuario.Endereco = registro["endereco"].ToString();
+        usuario.Numero = registro["numero"].ToString();
+
+        return usuario;
+    }
+
+    public void Inserir(UsuarioViewModel usuario)
+    {
+        HelperDAO.ExecutaProc("spInsert_Usuarios", CriaParametros(usuario));
+    }
+
+    public void Alterar(UsuarioViewModel usuario)
+    {
+        HelperDAO.ExecutaProc("spUpdate_Usuarios", CriaParametros(usuario));
+    }
+
+    public void Excluir(int id)
+    {
+        var p = new SqlParameter[]
+        {
+            new SqlParameter("id", id)
+        };
+        HelperDAO.ExecutaProc("spDelete", p);
+    }
+
+    protected override void SetTabela()
+    {
+        Tabela = "Usuarios";
+    }
+}
+
 
 }
