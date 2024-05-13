c72efef6eecb5af63717030f8c22a90fde4542e4
Kauê de Souza Silva <145780136+ConfuseKarma@users.noreply.github.com>
2024-05-13 15:34:23 -0300

    Update ProdutoController.cs

--- N1 2Bi - LP1/Controllers/ProdutoController.cs
+++ N1 2Bi - LP1/Controllers/ProdutoController.cs
@@ -78,21 +78,7 @@ namespace N1_2Bi___LP1.Controllers
 
         private void ValidaDados(ProdutoViewModel produto, string operacao)
         {
-            ModelState.Clear(); // Limpa os erros criados automaticamente pelo ASP.NET
-
-            ProdutoDAO dao = new ProdutoDAO();
-
-            // Verifica se o código já está em uso
-            if (operacao == "I" && dao.Consulta(produto.Id) != null)
-                ModelState.AddModelError("Id", "Código já está em uso.");
-
-            // Verifica se o produto existe para operações de alteração
-            if (operacao == "A" && dao.Consulta(produto.Id) == null)
-                ModelState.AddModelError("Id", "Produto não existe.");
-
-            // Verifica se o ID é válido
-            if (produto.Id <= 0)
-                ModelState.AddModelError("Id", "Id inválido!");
+            base.ValidaDados(produto, operacao); // Chama o método da classe base para validar dados básicos
 
             // Verifica se o nome foi preenchido
             if (string.IsNullOrEmpty(produto.Nome))
@@ -106,30 +92,13 @@ namespace N1_2Bi___LP1.Controllers
             if (produto.Preco <= 0)
                 ModelState.AddModelError("Preco", "O preço deve ser maior que zero.");
 
-           //Imagem será obrigatória apenas na inclusão.
-            //Na alteração iremos considerar a que já estava salva.
-            if (produto.ImagemEmBase64 == null && operacao == "I")
+            // Imagem será obrigatória apenas na inclusão.
+            // Na alteração iremos considerar a que já estava salva.
+            if (operacao == "I" && produto.ImagemEmBase64 == null)
                 ModelState.AddModelError("ImagemEmBase64", "Escolha uma imagem.");
             if (!string.IsNullOrEmpty(produto.ImagemEmBase64) && produto.ImagemEmBase64.Length / 1024 / 1024 >= 2)
                 ModelState.AddModelError("ImagemEmBase64", "Imagem limitada a 2 MB.");
-            if (ModelState.IsValid)
-            {
-                //Na alteração, se não foi informada a imagem, iremos manter a que já estava salva.
-                if (operacao == "A" && produto.ImagemEmBase64 == null)
-                {
-                    ProdutoViewModel prod = DAO.Consulta(produto.Id);
-                    produto.ImagemEmBase64 = prod.ImagemEmBase64;
-                }
-                else
-                {
-                    produto.Imagem = ConvertImageToByte(produto.ImagemEmBase64);
-                }
-            }
-
-                
         }
-
-
 
 
          public byte[] ConvertImageToByte(IFormFile file)
