f4b6a85d14d904ebe822dec16e851e17666fcc2b
Kauê de Souza Silva <145780136+ConfuseKarma@users.noreply.github.com>
2024-05-21 11:08:09 -0300

    Update PedidoViewModel.cs

--- N1 2Bi - LP1/Models/PedidoViewModel.cs
+++ N1 2Bi - LP1/Models/PedidoViewModel.cs
@@ -3,6 +3,7 @@ namespace N1_2Bi___LP1.Models
     public class PedidoViewModel : PadraoViewModel
     {
         public DateTime Data { get; set; }
+        public int UsuarioId { get; set; }
 
     }
 }
